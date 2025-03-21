e21d6820b3bf138ea2b2e29c8340710dc75afe05
IrynaKosiak7 <ira,oleonock2013@gmail.com>
2025-03-21 13:40:57 +0100

    upgraded SpringBoot in json format messages

--- src/main/java/lern/org/demo/controller/MainController.java
+++ src/main/java/lern/org/demo/controller/MainController.java
@@ -1,15 +1,18 @@
 package lern.org.demo.controller;
 
-import org.springframework.ui.Model;
+import lern.org.demo.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
     @GetMapping("/api/hello")
-    public String sayHello(){
-        return "Hello world";
+    public Greeting sayHello(){
+        return new Greeting("Hello world");
+    }
+    @GetMapping("/api/chao")
+    public Greeting sayChao(){
+        return new Greeting("Chao");
     }
 }
