96fbdabe57af2203943782b3e83d71e459500231
IrynaKosiak7 <ira,oleonock2013@gmail.com>
2025-03-28 14:04:23 +0100

    Erstellung frontend Applikation (React)

---
+++ src/frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <header className="App-header">
+            <img src="https://images.app.goo.gl/agZTPeu7Qu5DWJCN9" className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+        <a
+          className="demo App"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
